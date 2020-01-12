75e330cbb9fce4d9732653f489924de45b1b5ce9
zeek <zeek_beijing@126.com>
2020-01-12 15:16:49 +0800

    当项目启动的时候输出类加载器，说明了，通过java -jar启动SpringBoot项目时和直接通过idea运行SpringBoot项目时，使用的类加载器不一样

--- src/main/java/com/zeek/springbootlearn/MyApplication.java
+++ src/main/java/com/zeek/springbootlearn/MyApplication.java
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApplication {
 
     public static void main(String[] args) {
+
+        /// 在IDE中直接运行输出为：sun.misc.Launcher$AppClassLoader@18b4aac2，是应用类加载器！因为通过IDEA直接运行的方式
+        // 所有的类都位于classpath路径下，并且也不存在fatjar（即，jar包又含有jar包的问题）
+        // 当构建成jar文件，并且通过java -jar方式进行运行时，下面的代码输出为：
+        // org.springframework.boot.loader.LaunchedURLClassLoader@17f052a3，即，之前课程中所总结的SpringBoot自定义的一个
+        // 类加载器
+        System.out.println(MyApplication.class.getClassLoader());
+
         SpringApplication.run(MyApplication.class, args);
     }
 
